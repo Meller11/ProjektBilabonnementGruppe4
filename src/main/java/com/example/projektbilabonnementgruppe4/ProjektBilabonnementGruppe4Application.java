45bbfdb2cf7674e1304b7e182016556d08dc6d82
Meller11 <mael0003@stud.kea.dk>
2024-05-09 23:34:47 +0200

    Initial commit

---
+++ src/main/java/com/example/projektbilabonnementgruppe4/ProjektBilabonnementGruppe4Application.java
@@ -0,0 +1,13 @@
+package com.example.projektbilabonnementgruppe4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektBilabonnementGruppe4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektBilabonnementGruppe4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projektbilabonnementgruppe4/ProjektBilabonnementGruppe4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projektbilabonnementgruppe4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektBilabonnementGruppe4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
