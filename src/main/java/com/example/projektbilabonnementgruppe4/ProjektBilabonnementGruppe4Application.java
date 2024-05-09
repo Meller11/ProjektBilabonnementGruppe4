e0edd7a88e738b3b9afbd58da76090ba30b2b3f8
Meller11 <mael0003@stud.kea.dk>
2024-05-09 23:59:33 +0200

    application.properties

--- src/main/java/com/example/projektbilabonnementgruppe4/ProjektBilabonnementGruppe4Application.java
+++ src/main/java/com/example/projektbilabonnementgruppe4/ProjektBilabonnementGruppe4Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjektBilabonnementGruppe4Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjektBilabonnementGruppe4Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektBilabonnementGruppe4Application.class, args);
+    }
 
 }
--- src/test/java/com/example/projektbilabonnementgruppe4/ProjektBilabonnementGruppe4ApplicationTests.java
+++ src/test/java/com/example/projektbilabonnementgruppe4/ProjektBilabonnementGruppe4ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProjektBilabonnementGruppe4ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
