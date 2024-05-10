26e1d4581a3f3405631836d06cdc24139ef599db
Meller11 <mael0003@stud.kea.dk>
2024-05-11 01:49:44 +0200

    Tilføjet controller, model, service, repository og HTML. CSS, update og delete in progress

---
+++ src/main/java/com/example/projektbilabonnementgruppe4/model/RentalAgreement.java
@@ -0,0 +1,105 @@
+package com.example.projektbilabonnementgruppe4.model;
+
+import java.time.LocalDate;
+
+public class RentalAgreement {
+    private int contractId;
+    private int employeeId;
+    private int carId;
+    private String contractNumber;
+    private String pickupLocation;
+    private LocalDate contractStartDate;
+    private LocalDate contractEndDate;
+    private String contractType;
+    private double contractMonthlyFee;
+
+    public RentalAgreement() {
+    }
+
+    public RentalAgreement(int contractId, int employeeId, int carId, String contractNumber, String pickupLocation, LocalDate contractStartDate, LocalDate contractEndDate, String contractType, double contractMonthlyFee) {
+        this.contractId = contractId;
+        this.employeeId = employeeId;
+        this.carId = carId;
+        this.contractNumber = contractNumber;
+        this.pickupLocation = pickupLocation;
+        this.contractStartDate = contractStartDate;
+        this.contractEndDate = contractEndDate;
+        this.contractType = contractType;
+        this.contractMonthlyFee = contractMonthlyFee;
+    }
+
+    public double getContractMonthlyFee() {
+        return contractMonthlyFee;
+    }
+
+    public void setContractMonthlyFee(double contractMonthlyFee) {
+        this.contractMonthlyFee = contractMonthlyFee;
+    }
+
+    public int getContractId() {
+        return contractId;
+    }
+
+    public void setContractId(int contractId) {
+        this.contractId = contractId;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getCarId() {
+        return carId;
+    }
+
+    public void setCarId(int carId) {
+        this.carId = carId;
+    }
+
+    public String getPickupLocation() {
+        return pickupLocation;
+    }
+
+    public void setPickupLocation(String pickupLocation) {
+        this.pickupLocation = pickupLocation;
+    }
+
+    public String getContractNumber() {
+        return contractNumber;
+    }
+
+    public void setContractNumber(String contractNumber) {
+        this.contractNumber = contractNumber;
+    }
+
+    public LocalDate getContractStartDate() {
+        return contractStartDate;
+    }
+
+    public void setContractStartDate(LocalDate contractStartDate) {
+        this.contractStartDate = contractStartDate;
+    }
+
+    public LocalDate getContractEndDate() {
+        return contractEndDate;
+    }
+
+    public void setContractEndDate(LocalDate contractEndDate) {
+        this.contractEndDate = contractEndDate;
+    }
+
+    public String getContractType() {
+        return contractType;
+    }
+
+    public void setContractType(String contractType) {
+        this.contractType = contractType;
+    }
+}
+
+
+
---
+++ src/main/java/com/example/projektbilabonnementgruppe4/service/RentalAgreementService.java
@@ -0,0 +1,37 @@
+package com.example.projektbilabonnementgruppe4.service;
+
+import com.example.projektbilabonnementgruppe4.model.RentalAgreement;
+import com.example.projektbilabonnementgruppe4.repository.RentalAgreementRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RentalAgreementService {
+
+    private final RentalAgreementRepository rentalAgreementRepository;
+
+    public RentalAgreementService(RentalAgreementRepository rentalAgreementRepository) {
+        this.rentalAgreementRepository = rentalAgreementRepository;
+    }
+
+    public RentalAgreement createRentalAgreement(RentalAgreement rentalAgreement) {
+        return rentalAgreementRepository.createRentalAgreement(rentalAgreement);
+    }
+    public List<RentalAgreement> getAllRentalAgreements() {
+        return rentalAgreementRepository.getAllRentalAgreements();
+    }
+
+    public RentalAgreement getRentalAgreement(int rentalAgreementId) {
+        return rentalAgreementRepository.getRentalAgreement(rentalAgreementId);
+    }
+
+    public RentalAgreement updateRentalAgreement(RentalAgreement rentalAgreement) {
+        return rentalAgreementRepository.updateRentalAgreement(rentalAgreement);
+    }
+
+    public void deleteRentalAgreement(int rentalAgreementId) {
+        rentalAgreementRepository.deleteRentalAgreement(rentalAgreementId);
+    }
+
+}
