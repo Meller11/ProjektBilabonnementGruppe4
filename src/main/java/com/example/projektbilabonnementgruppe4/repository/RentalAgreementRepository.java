39cb6c26626bb4813b679799e9225d654f6dd504
Meller11 <mael0003@stud.kea.dk>
2024-05-19 13:02:46 +0200

    Ændret contract til rental_agreement i databasen samt rettet i RentalAgreementRepository.java

--- src/main/java/com/example/projektbilabonnementgruppe4/repository/RentalAgreementRepository.java
+++ src/main/java/com/example/projektbilabonnementgruppe4/repository/RentalAgreementRepository.java
@@ -20,50 +20,50 @@ public class RentalAgreementRepository {
     }
 
     public void createRentalAgreement(RentalAgreement rentalAgreement) {
-        String sql = "INSERT INTO contract (employee_id, car_id, contract_number, pickup_location, contract_start_date, contract_end_date, contract_type, contract_monthly_fee, mileage_per_month) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO rental_agreement (employee_id, car_id, contract_number, pickup_location, contract_start_date, contract_end_date, contract_type, contract_monthly_fee, mileage_per_month) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
         jdbcTemplate.update(sql, rentalAgreement.getEmployeeId(), rentalAgreement.getCarId(), rentalAgreement.getContractNumber(), rentalAgreement.getPickupLocation(), rentalAgreement.getContractStartDate(), rentalAgreement.getContractEndDate(), rentalAgreement.getContractType(), rentalAgreement.getContractMonthlyFee(),rentalAgreement.getMileagePerMonth());
     }
 
     public RentalAgreement getRentalAgreement(int rentalAgreementId) {
-        String sql = "SELECT * FROM contract WHERE contract_id = ?";
+        String sql = "SELECT * FROM rental_agreement WHERE contract_id = ?";
         return jdbcTemplate.queryForObject(sql, new Object[]{rentalAgreementId}, new BeanPropertyRowMapper<>(RentalAgreement.class));
     }
 
     public List<RentalAgreement> getAllRentalAgreements() {
-        String sql = "SELECT * FROM contract";
+        String sql = "SELECT * FROM rental_agreement";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(RentalAgreement.class));
     }
 
     public void updateRentalAgreement(RentalAgreement rentalAgreement) {
-        String sql = "UPDATE contract SET employee_id = ?, contract_number = ?, pickup_location = ?, contract_start_date = ?, contract_end_date = ?, contract_type = ?, contract_monthly_fee = ?, mileage_per_month = ? WHERE contract_id = ?";
+        String sql = "UPDATE rental_agreement SET employee_id = ?, contract_number = ?, pickup_location = ?, contract_start_date = ?, contract_end_date = ?, contract_type = ?, contract_monthly_fee = ?, mileage_per_month = ? WHERE contract_id = ?";
         jdbcTemplate.update(sql, rentalAgreement.getEmployeeId(), rentalAgreement.getContractNumber(), rentalAgreement.getPickupLocation(), rentalAgreement.getContractStartDate(), rentalAgreement.getContractEndDate(), rentalAgreement.getContractType(),rentalAgreement.getContractMonthlyFee(), rentalAgreement.getMileagePerMonth(), rentalAgreement.getContractId());
     }
 
     public void deleteRentalAgreement(int rentalAgreementId) {
-        String sql = "DELETE FROM contract WHERE contract_id = ?";
+        String sql = "DELETE FROM rental_agreement WHERE contract_id = ?";
         jdbcTemplate.update(sql, rentalAgreementId);
     }
 
 
     public List<RentedCar> getAllRentedCars() {
-        String sql = "SELECT c.car_id, c.frame_number, c.brand, c.model, co.contract_id, co.contract_number, co.pickup_location, co.contract_start_date, co.contract_end_date " +
-                "FROM contract co " +
-                "JOIN car c ON co.car_id = c.car_id";
+        String sql = "SELECT c.car_id, c.frame_number, c.brand, c.model, ra.contract_id, ra.contract_number, ra.pickup_location, ra.contract_start_date, ra.contract_end_date " +
+                "FROM rental_agreement ra " +
+                "JOIN car c ON ra.car_id = c.car_id";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(RentedCar.class));
     }
 
     public int getAverageMonthlyFee(){
-        String sql = "SELECT AVG(contract_monthly_fee) FROM contract;";
+        String sql = "SELECT AVG(contract_monthly_fee) FROM rental_agreement;";
         return jdbcTemplate.queryForObject(sql, Integer.class);
     }
 
     public double getTotalMileageOfContracts(){
-        String sql = "SELECT SUM(mileage_per_month) FROM contract;";
+        String sql = "SELECT SUM(mileage_per_month) FROM rental_agreement;";
         return jdbcTemplate.queryForObject(sql, Double.class);
     }
 
     public double getMileageOfContract(int contractID){
-        String sql = "SELECT mileage_per_month FROM contract WHERE contract_id = ?";
+        String sql = "SELECT mileage_per_month FROM rental_agreement WHERE contract_id = ?";
         return jdbcTemplate.queryForObject(sql, Double.class, contractID);
     }
 
