9cf46b82e8f4d367e38138d0ba20f41c715fc4a2
LasseFoxy <lafo0001@stud.kea.dk>
2024-05-28 13:31:44 +0200

    Rettet fejl der gør at man fik en fejl på startsiden første gang man logger ind. Den henter et session et sessionID som gør at headeren kan bruge den bruger der er logget ind, selvom det ligner at variablen ikke bliver brugt

--- src/main/java/com/example/projektbilabonnementgruppe4/controller/HomeController.java
+++ src/main/java/com/example/projektbilabonnementgruppe4/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.projektbilabonnementgruppe4.controller;
 
+import com.example.projektbilabonnementgruppe4.model.Employee;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,7 +10,8 @@ public class HomeController {
 
     //Viser startsiden
     @GetMapping("/")
-    public String home() {
+    public String home(HttpSession session) {
+        Employee loggedInUser = (Employee) session.getAttribute("loggedInUser");
         return "home";
     }
 }
